f7487fd88d20a1dfd21cdc7ba50ded4d560f3d7d
Tanisha Purnell <tanishad93@gmail.com>
2017-12-30 15:28:15 -0700

    fixed issue with stylesheets linking to index page

--- d2/index2.php
+++ d2/index2.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>JS + CSS Clock</title>
-		<link rel="stylesheet" href="styles">
+		<link rel="stylesheet" href="styles2/styles2.css">
 		<link rel="script" href="script.js">
 	</head>
 	<body>
